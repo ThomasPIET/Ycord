d5de9dd7cfa2d389f007dbf673994d4dbb3aea03
Pablo <pablo.huys@ynov.com>
2025-02-18 15:55:12 +0100

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("YTsap - Peer2Peer Client");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+

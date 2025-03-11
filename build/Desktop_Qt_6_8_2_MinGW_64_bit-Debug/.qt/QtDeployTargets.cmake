c40424eb70ecf2973542f6e8ac559faf42b3b661
IF0rst <124600941+IF0rst@users.noreply.github.com>
2025-03-11 12:19:46 +0100

    Delete build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_YTsap_FILE C:/Users/pablo/Desktop/Ycord/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/YTsap.exe)
-set(__QT_DEPLOY_TARGET_YTsap_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_YTsap_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6WebSockets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
